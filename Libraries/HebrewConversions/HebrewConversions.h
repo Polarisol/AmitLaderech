693b10ce115ad6d52dc2fff29451bff19050f0a5
Tal Alon <alon.tal@gmail.com>
2017-01-05 23:46:02 +0200

    Shrinked down the header for Hebrew conversions

--- Libraries/HebrewConversions/HebrewConversions.h
+++ Libraries/HebrewConversions/HebrewConversions.h
@@ -1,15 +1,3 @@
-// Utility function used by 'HebrewConverter' functions to add an array value
-void HebrewConverter_expand(char arr[], int new_index, int size);
-
-
-// Utility function used by 'HebrewConverter' functions to delete an array value
-void HebrewConverter_collapse(char arr[], int del_index, int size);
-
-
-// Utility function used by 'HebrewConverter' functions to find a hebrew letter index in an array
-int HebrewConverter_find_letter_index(char wrong[], char val);
-
-
 // HEBREW Convertor: Converts a string from <UTF-8 without BOM> to <ISO 8859-8> / <Windows-1255>
 // Should only be applied to English-Hebrew strings (the effect on other languages is no known)
 // If the string is already in ISO or Window format the Hebrew letters remain OK
