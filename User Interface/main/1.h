e8cf6047bc09dbc0e413b3d9510b1210b6197a3f
shay yitshak <shay yitshak>
2017-01-15 22:59:16 +0200

    new panels

--- User Interface/main/1.h
+++ User Interface/main/1.h
@@ -13,7 +13,15 @@
 
      /* Panels and Controls: */
 
-#define  P_MAIN                           1       /* callback function: Exit_Func */
+#define  P_ACTIVITY                       1
+#define  P_ACTIVITY_MANHE_4_BOTTON        2       /* control type: command, callback function: (none) */
+#define  P_ACTIVITY_MANHE_3_BOTTON        3       /* control type: command, callback function: (none) */
+#define  P_ACTIVITY_MANHE_2_BOTTON        4       /* control type: command, callback function: (none) */
+#define  P_ACTIVITY_MANHE_1_BOTTON        5       /* control type: command, callback function: (none) */
+#define  P_ACTIVITY_ADD_MANHE_BUTTON      6       /* control type: command, callback function: (none) */
+#define  P_ACTIVITY_COMMANDBUTTON         7       /* control type: command, callback function: (none) */
+
+#define  P_MAIN                           2       /* callback function: Exit_Func */
 #define  P_MAIN_STRING                    2       /* control type: string, callback function: (none) */
 #define  P_MAIN_STRING_2                  3       /* control type: string, callback function: (none) */
 #define  P_MAIN_COMMANDBUTTON_3           4       /* control type: command, callback function: (none) */
@@ -24,7 +32,35 @@
 #define  P_MAIN_COMMANDBUTTON_4           9       /* control type: command, callback function: (none) */
 #define  P_MAIN_PICTURE                   10      /* control type: picture, callback function: (none) */
 
-#define  P_NEW_AMIT                       2
+#define  P_MANHE                          3
+#define  P_MANHE_STRING_9                 2       /* control type: string, callback function: (none) */
+#define  P_MANHE_STRING_8                 3       /* control type: string, callback function: (none) */
+#define  P_MANHE_STRING_5                 4       /* control type: string, callback function: (none) */
+#define  P_MANHE_STRING_3                 5       /* control type: string, callback function: (none) */
+#define  P_MANHE_STRING_2                 6       /* control type: string, callback function: (none) */
+#define  P_MANHE_STRING                   7       /* control type: string, callback function: (none) */
+#define  P_MANHE_PICTURE                  8       /* control type: picture, callback function: (none) */
+#define  P_MANHE_COMMANDBUTTON            9       /* control type: command, callback function: (none) */
+#define  P_MANHE_COMMANDBUTTON_2          10      /* control type: command, callback function: (none) */
+#define  P_MANHE_COMMANDBUTTON_3          11      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_15                12      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_14                13      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_13                14      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_12                15      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_11                16      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_10                17      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_9                 18      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_8                 19      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_7                 20      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_6                 21      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_5                 22      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_4                 23      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_3                 24      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_2                 25      /* control type: command, callback function: (none) */
+#define  P_MANHE_MENTOR_1                 26      /* control type: command, callback function: (none) */
+#define  P_MANHE_COMMANDBUTTON_4          27      /* control type: command, callback function: (none) */
+
+#define  P_NEW_AMIT                       4
 #define  P_NEW_AMIT_STRING_7              2       /* control type: string, callback function: (none) */
 #define  P_NEW_AMIT_STRING_6              3       /* control type: string, callback function: (none) */
 #define  P_NEW_AMIT_STRING_9              4       /* control type: string, callback function: (none) */
@@ -38,6 +74,38 @@
 #define  P_NEW_AMIT_COMMANDBUTTON_2       12      /* control type: command, callback function: (none) */
 #define  P_NEW_AMIT_PICTURE               13      /* control type: picture, callback function: (none) */
 #define  P_NEW_AMIT_TEXTMSG               14      /* control type: textMsg, callback function: (none) */
+
+#define  P_NEW_MENT                       5
+#define  P_NEW_MENT_STRING_7              2       /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING_6              3       /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING_9              4       /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING_10             5       /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING_8              6       /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING_5              7       /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING_4              8       /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING_3              9       /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING_2              10      /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_STRING                11      /* control type: string, callback function: (none) */
+#define  P_NEW_MENT_COMMANDBUTTON         12      /* control type: command, callback function: (none) */
+#define  P_NEW_MENT_COMMANDBUTTON_2       13      /* control type: command, callback function: (none) */
+#define  P_NEW_MENT_PICTURE               14      /* control type: picture, callback function: (none) */
+#define  P_NEW_MENT_TEXTMSG               15      /* control type: textMsg, callback function: (none) */
+
+#define  P_SOLIDER                        6
+#define  P_SOLIDER_STRING_7               2       /* control type: string, callback function: (none) */
+#define  P_SOLIDER_STRING_6               3       /* control type: string, callback function: (none) */
+#define  P_SOLIDER_STRING_10              4       /* control type: string, callback function: (none) */
+#define  P_SOLIDER_STRING_9               5       /* control type: string, callback function: (none) */
+#define  P_SOLIDER_STRING_8               6       /* control type: string, callback function: (none) */
+#define  P_SOLIDER_STRING_5               7       /* control type: string, callback function: (none) */
+#define  P_SOLIDER_STRING_3               8       /* control type: string, callback function: (none) */
+#define  P_SOLIDER_STRING_2               9       /* control type: string, callback function: (none) */
+#define  P_SOLIDER_STRING                 10      /* control type: string, callback function: (none) */
+#define  P_SOLIDER_PICTURE                11      /* control type: picture, callback function: (none) */
+#define  P_SOLIDER_COMMANDBUTTON          12      /* control type: command, callback function: (none) */
+#define  P_SOLIDER_COMMANDBUTTON_2        13      /* control type: command, callback function: (none) */
+#define  P_SOLIDER_RINGSLIDE              14      /* control type: slide, callback function: (none) */
+#define  P_SOLIDER_LED                    15      /* control type: LED, callback function: (none) */
 
 
      /* Control Arrays: */
