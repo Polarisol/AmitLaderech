cd67adfa3800774bf9c5468cf6388cdf4da20640
gabriel <gabriel.ladzaretti@gmail.com>
2017-02-23 19:22:22 +0200

    function has been redone from scratch,can now load entire CSV file into cvi.

--- Libraries/Data_analytics/test.c
+++ Libraries/Data_analytics/test.c
@@ -1,28 +1,26 @@
 #include <cvirte.h>
 #include <userint.h>
+#include <stdio.h>
 #include "test.h"
-#include "Func_Header.h"
 #include "HebrewConversions.h"
-#define SIZE 300
-#define PATH "test1.csv"
-#define RINGDEF USERINT_FIELD_RING1
-
-static int userint;
+#include "Func_Header.h"
+static int mainPanel;
+int column_num,rows_num;
 
 int main (int argc, char *argv[])
 {
 	if (InitCVIRTE (0, argv, 0) == 0)
 		return -1;	/* out of memory */
-	if ((userint = LoadPanel (0, "test.uir", USERINT)) < 0)
+	if ((mainPanel = LoadPanel (0, "test.uir", MAIN_PANEL)) < 0)
 		return -1;
-	DisplayPanel (userint);
+	DisplayPanel (mainPanel);
 	RunUserInterface ();
-	DiscardPanel (userint);
+	DiscardPanel (mainPanel);
 	return 0;
 }
 
-int CVICALLBACK globalfunc (int panel, int event, void *callbackData,
-							int eventData1, int eventData2)
+int CVICALLBACK main_func (int panel, int event, void *callbackData,
+						   int eventData1, int eventData2)
 {
 	switch (event)
 	{
@@ -39,41 +37,60 @@ int CVICALLBACK globalfunc (int panel, int event, void *callbackData,
 	return 0;
 }
 
-int CVICALLBACK button (int panel, int control, int event,
-						void *callbackData, int eventData1, int eventData2)
+int CVICALLBACK load_csv (int panel, int control, int event,
+						  void *callbackData, int eventData1, int eventData2)
 {
-	char fromcsv[SIZE],field[SIZE],originalheb[SIZE];
-	int status,chosen_row,i=0;
+	int MAX_SIZE = 10000;
+	char line[MAX_SIZE];
+	char file_name[300],header[300];
+	char** list = NULL;
+	int num_of_values,status;
+	FILE *Stream;
+
+
 	switch (event)
 	{
-			/*case EVENT_COMMIT:
-				CSVParser_GetFieldFromRecord("test2.csv",2,"name",fromcsv);
-				SetCtrlVal (userint, USERINT_TEXT1, fromcsv);
-
-				break;*/
-
 		case EVENT_COMMIT:
-
-			for(i=0; i<=4; i++)
+			ClearListCtrl (mainPanel, MAIN_PANEL_RING);
+			GetCtrlVal (mainPanel,  MAIN_PANEL_CSV_PATH, file_name);
+			Stream = fopen(file_name,"r");
+			if(Stream)
 			{
-				//HebrewConverter_convertHebrewUTF8toISO(originalheb);
-				GetCtrlVal (userint, USERINT_CHOOSE_ROW, &chosen_row);
-				//GetCtrlVal (userint, USERINT_TEXTMSG,originalheb);
-				//SetCtrlVal (userint, USERINT_TEXTMSG_2,originalheb);
-				SetCtrlAttribute (userint, RINGDEF, ATTR_CTRL_INDEX, i);
-				GetCtrlAttribute (userint, RINGDEF, ATTR_CTRL_VAL,originalheb);
-				HebrewConverter_convertHebrewISOtoUTF8(originalheb); 
-				status = CSVParser_GetFieldFromRecord(PATH,chosen_row, originalheb, fromcsv);
-				if(status==1)
+				fgetcsvl(line,MAX_SIZE,Stream);
+				list = CSV_Analyzer(line, &num_of_values);
+				column_num=num_of_values;
+				for(int i=0 ; i<num_of_values; i++)
 				{
-					HebrewConverter_convertHebrewUTF8toISO(fromcsv);
-					SetCtrlVal (userint, USERINT_TEXT1, fromcsv);
-					SetTableCellVal (userint, USERINT_TABLE, MakePoint(i+1,chosen_row),fromcsv );
+					HebrewConverter_convertHebrewUTF8toISO(list[i]);
+					InsertListItem (mainPanel, MAIN_PANEL_RING, i, list[i], i);
+					GetNumTableRows (mainPanel, MAIN_PANEL_CSV_TABLE, &rows_num);
+					if (!rows_num)
+						InsertTableRows (mainPanel, MAIN_PANEL_CSV_TABLE, -1, 1, VAL_CELL_STRING);
+					InsertTableColumns (mainPanel, MAIN_PANEL_CSV_TABLE, -1, 1, VAL_CELL_STRING);
+					SetTableCellVal (mainPanel, MAIN_PANEL_CSV_TABLE, MakePoint(i+1,1),list[i] );
 				}
-				else if (!status)
-					SetCtrlVal (userint, USERINT_TEXT1, "Error");
 			}
-
+			fclose(Stream);
+			rows_num=CSVParser_GetNumberOfRecords(file_name);
+			for (int i=1; i<rows_num+1; i++)
+			{
+				InsertTableRows (mainPanel, MAIN_PANEL_CSV_TABLE, -1, 1, VAL_CELL_STRING);
+				for (int j=1; j<column_num+1; j++)
+				{
+					GetLabelFromIndex (mainPanel, MAIN_PANEL_RING, j-1, header);
+					HebrewConverter_convertHebrewISOtoUTF8(header);
+					status = CSVParser_GetFieldFromRecord(file_name, i, header, line);
+					if(status==1)
+					{
+						HebrewConverter_convertHebrewUTF8toISO(line);
+						SetTableCellVal (mainPanel, MAIN_PANEL_CSV_TABLE, MakePoint(j,i+1),line );
+					}
+					else
+					{
+						MessagePopup ("error", "error");
+					}
+				}
+			}
 			break;
 	}
 	return 0;
--- Libraries/Data_analytics/test.h
+++ Libraries/Data_analytics/test.h
@@ -13,24 +13,20 @@
 
      /* Panels and Controls: */
 
-#define  USERINT                          1       /* callback function: globalfunc */
-#define  USERINT_PICTURE                  2       /* control type: picture, callback function: (none) */
-#define  USERINT_PICTURE_6                3       /* control type: picture, callback function: (none) */
-#define  USERINT_PICTURE_2                4       /* control type: picture, callback function: (none) */
-#define  USERINT_PICTURE_3                5       /* control type: picture, callback function: (none) */
-#define  USERINT_PICTURE_5                6       /* control type: picture, callback function: (none) */
-#define  USERINT_PICTURE_4                7       /* control type: picture, callback function: (none) */
-#define  USERINT_SPLITTER                 8       /* control type: splitter, callback function: (none) */
-#define  USERINT_BUTTON                   9       /* control type: command, callback function: button */
-#define  USERINT_TEXT1                    10      /* control type: textMsg, callback function: (none) */
-#define  USERINT_TEXTMSG_2                11      /* control type: textMsg, callback function: (none) */
-#define  USERINT_RING                     12      /* control type: ring, callback function: (none) */
-#define  USERINT_CHOOSE_ROW               13      /* control type: numeric, callback function: (none) */
-#define  USERINT_FIELD_RING3              14      /* control type: ring, callback function: (none) */
-#define  USERINT_FIELD_RING2              15      /* control type: ring, callback function: (none) */
-#define  USERINT_FIELD_RING1              16      /* control type: ring, callback function: (none) */
-#define  USERINT_TABLE                    17      /* control type: table, callback function: (none) */
-#define  USERINT_TEXTMSG                  18      /* control type: textMsg, callback function: (none) */
+#define  MAIN_PANEL                       1       /* callback function: main_func */
+#define  MAIN_PANEL_PICTURE               2       /* control type: picture, callback function: (none) */
+#define  MAIN_PANEL_PICTURE_6             3       /* control type: picture, callback function: (none) */
+#define  MAIN_PANEL_PICTURE_2             4       /* control type: picture, callback function: (none) */
+#define  MAIN_PANEL_PICTURE_3             5       /* control type: picture, callback function: (none) */
+#define  MAIN_PANEL_PICTURE_5             6       /* control type: picture, callback function: (none) */
+#define  MAIN_PANEL_PICTURE_4             7       /* control type: picture, callback function: (none) */
+#define  MAIN_PANEL_SPLITTER              8       /* control type: splitter, callback function: (none) */
+#define  MAIN_PANEL_COMMANDBUTTON         9       /* control type: command, callback function: (none) */
+#define  MAIN_PANEL_COMMANDBUTTON_2       10      /* control type: command, callback function: (none) */
+#define  MAIN_PANEL_CSV_TABLE             11      /* control type: table, callback function: (none) */
+#define  MAIN_PANEL_LOAD                  12      /* control type: command, callback function: load_csv */
+#define  MAIN_PANEL_RING                  13      /* control type: ring, callback function: (none) */
+#define  MAIN_PANEL_CSV_PATH              14      /* control type: string, callback function: (none) */
 
 
      /* Control Arrays: */
@@ -45,8 +41,8 @@
 
      /* Callback Prototypes: */
 
-int  CVICALLBACK button(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
-int  CVICALLBACK globalfunc(int panel, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK load_csv(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK main_func(int panel, int event, void *callbackData, int eventData1, int eventData2);
 
 
 #ifdef __cplusplus
