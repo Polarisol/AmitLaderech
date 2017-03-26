c1284731fe237842376a0b36ac611b59db85da05
gabriel <gabriel.ladzaretti@gmail.com>
2017-03-26 10:18:16 +0300

    added mentor/soldier ring

--- Libraries/analytics_functions_demo/analytics_functions_demo.c
+++ Libraries/analytics_functions_demo/analytics_functions_demo.c
@@ -1,7 +1,9 @@
-#include <cvirte.h>		
+#include <cvirte.h>	
+#include <stdio.h>    
 #include <userint.h>
 #include "analytics_functions_demo.h"
-
+#include "analytics_functions.h"
+#define SIZE 300
 static int panelHandle;
 
 int main (int argc, char *argv[])
@@ -29,6 +31,29 @@ int CVICALLBACK demo (int panel, int event, void *callbackData,
 			break;
 		case EVENT_CLOSE:
 			QuitUserInterface (0);
+			break;
+	}
+	return 0;
+}
+
+int CVICALLBACK extract (int panel, int control, int event,
+						 void *callbackData, int eventData1, int eventData2)
+{
+	char **names=NULL,**sorted=NULL,path[SIZE],name_column[SIZE];
+	int array_size;
+	switch (event)
+	{
+		case EVENT_COMMIT:
+			GetCtrlVal (panelHandle, PANEL_PATH, path);
+			GetCtrlVal (panelHandle, PANEL_EXTRACT_DATA, name_column);
+			names=extractnames(path,name_column, &array_size); 
+			sorted=removeduplicates (names,array_size,&array_size);
+			DeleteListItem (panelHandle, PANEL_MENTORS, 0, -1);
+			for (int i=0;i<array_size;i++)
+			{
+				HebrewConverter_convertHebrewUTF8toISO(sorted[i]);
+				InsertListItem (panelHandle, PANEL_MENTORS, -1, sorted[i],sorted[i]);
+			}
 			break;
 	}
 	return 0;
--- Libraries/analytics_functions_demo/analytics_functions_demo.h
+++ Libraries/analytics_functions_demo/analytics_functions_demo.h
@@ -14,6 +14,9 @@
      /* Panels and Controls: */
 
 #define  PANEL                            1       /* callback function: demo */
+#define  PANEL_PATH                       2       /* control type: string, callback function: (none) */
+#define  PANEL_MENTORS                    3       /* control type: ring, callback function: (none) */
+#define  PANEL_EXTRACT_DATA               4       /* control type: ring, callback function: extract */
 
 
      /* Control Arrays: */
@@ -29,6 +32,7 @@
      /* Callback Prototypes: */
 
 int  CVICALLBACK demo(int panel, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK extract(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 
 
 #ifdef __cplusplus
